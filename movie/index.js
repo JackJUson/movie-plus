432170ede404866c81877aa3921f6674f031edf0
Jackson <z5397791@ad.unsw.edu.au>
2022-12-21 21:44:34 +1100

    Add second fetch function for movie genre in main function

--- movie/index.js
+++ movie/index.js
@@ -7,10 +7,27 @@ async function main() {
   );
   const moviesData = await movies.json();
 
+  const genres = await fetch(
+    "https://api.themoviedb.org/3/genre/movie/list?api_key=2269437d4f4dc8cefddc0b2f852029a4&language=en-US"
+  );
+  const genresData = await genres.json();
+
   const movieListElement = document.querySelector(".movies__list");
   movieListElement.innerHTML = moviesData.results
-    .map((movie) => movieHTML(movie))
+    .map((movie) => movieHTML(movie, genresData))
     .join("");
 }
 
 main();
+
+function movieHTML(movie, genresData) {
+  return `<div class="movie">
+                <div class="movie_-container">
+                    <h3>${movie.title}</h3>
+                    <p>${showMovieGenre(movie.genre_ids, genresData)}</p>
+                    <img src=${
+                      "https://image.tmdb.org/t/p/w500" + movie.poster_path
+                    } alt="">
+                </div>
+            </div>`;
+}
