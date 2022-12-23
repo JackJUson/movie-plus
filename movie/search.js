0cb6aac506e1a1efbb0489cac5db634d0ed00333
Jackson <z5397791@ad.unsw.edu.au>
2022-12-23 18:55:32 +1100

    Written implementation for renderSearchedMovies function to change api on search value

---
+++ movie/search.js
@@ -0,0 +1,16 @@
+// API 1: https://api.themoviedb.org/3/search/movie?api_key=2269437d4f4dc8cefddc0b2f852029a4&language=en-US&query=REPLACETHIS&page=1&include_adult=false
+
+let searchInput;
+
+const input = document.getElementById("search__input");
+const button = document.getElementById("search__button");
+
+button.addEventListener('click', renderSearchedMovies);
+
+function renderSearchedMovies() {
+  searchInput = input.value;
+  mainApi = `https://api.themoviedb.org/3/search/movie?api_key=2269437d4f4dc8cefddc0b2f852029a4&language=en-US&query=${searchInput}&page=1&include_adult=false`;
+  moviesData = null;
+  renderMovies();
+}
+
